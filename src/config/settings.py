616d0e4b2e1ad878827a4fcdf17afaa6854c2840
Eduardo Lopes <eduardopnm@gmail.com>
2026-02-14 10:28:41 -0400

    refatoração: removendo duplicada src estrutura e limpando gitignore

--- src/config/settings.py
+++
@@ -1,5 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENDOTA_BASE_URL = "https://api.opendota.com/api"
--- src/db/duckdb.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-from pathlib import Path
-
-DB_PATH = Path("data/dota.duckdb")
-
-
-def get_connection():
-    return duckdb.connect(DB_PATH)
